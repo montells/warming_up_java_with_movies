cba532cd1f62a5d3a2d556985fdf18904ac0581d
Michel Sánchez Montells <michel.sanchez_montells@wassermeloni.de>
2026-08-04 00:07:39 +0200

    Assinging objects

---
+++ src/rocali/play/MainStackHeap.java
@@ -0,0 +1,18 @@
+package rocali.play;
+
+import rocali.play.content.Movie;
+
+public class MainStackHeap {
+    public static void main(String[] args) {
+        Movie lion_king = new Movie("The Lion King", 120, "Cartoon");
+        Movie harry_potter = new Movie("Harry Potter", 120, "Fantasy");
+
+        // referencing same place in heap
+        lion_king = harry_potter;
+
+        lion_king.title = "Pulp Fiction";
+
+        System.out.println("lion " + lion_king.title);
+        System.out.println("harry " + harry_potter.title);
+    }
+}
