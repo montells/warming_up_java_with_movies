b39dcef33abd6942a11c89de4f68666fa07c9136
Michel Sánchez Montells <michel.sanchez_montells@wassermeloni.de>
2026-08-03 07:02:28 +0200

    constants with final

--- src/rocali/play/Main.java
+++ src/rocali/play/Main.java
@@ -11,8 +11,12 @@ import java.time.Month;
 import java.util.Scanner;
 
 public class Main {
+    public static final String  VERSION = "1.0.0";
+
     public static void main(String[] args) {
-        System.out.println("Warming 🏃 up JAVA! With a little project for managing Movies 📽");
+        System.out.println("Warming 🏃 up JAVA!");
+        System.out.println("A little project for managing Movies 📽.");
+        System.out.println("v" + VERSION);
         showMovie();
 //        extracted();
     }
--- src/rocali/play/utils/ScannerUtils.java
+++ src/rocali/play/utils/ScannerUtils.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 
 public class ScannerUtils {
 
-    public static Scanner scanner = new Scanner(System.in);
+    public static final Scanner SCANNER = new Scanner(System.in);
 
     public static String getText(String message){
         System.out.println(message + ": ");
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 
     public static int getInt(String message){
         System.out.println(message + ": ");
 
-        int number = scanner.nextInt();
-        scanner.nextLine();
+        int number = SCANNER.nextInt();
+        SCANNER.nextLine();
         return number;
     }
 
     public static double getDecimal(String message){
         System.out.println(message + ": ");
 
-        double number = scanner.nextDouble();
-        scanner.nextLine();
+        double number = SCANNER.nextDouble();
+        SCANNER.nextLine();
         return number;
     }
 }
